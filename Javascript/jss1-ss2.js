47902fa983b57e238ac50eee8f7594883ceccd9d
dwasit <110547323+dwasit@users.noreply.github.com>
2022-09-08 13:28:47 +0100

    Add files via upload

---
+++ Javascript/jss1-ss2.js
@@ -0,0 +1,9 @@
+const cayleycollege1 = "jss1"
+const cayleycollege2 = "jss2"
+const cayleycollege3 = "jss3"
+const cayleycollege4 = "sss1"
+const cayleycollege5 = "sss2"
+
+const cayleycollege = ["jss1","jss2","jss3","sss1","sss2"]
+
+console.log(cayleycollege);
